4794c0b98210b5e7b41b126733e610ed71de9f75
NeverSayNever-Q <69444665+NeverSayNever-Q@users.noreply.github.com>
2022-02-07 08:16:27 +0800

    项目初始化

---
+++ src/test/java/com/nsn/companion/CompanionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nsn.companion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompanionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
