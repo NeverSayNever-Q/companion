b9029a10242672244b7a22f4dcb4224d6dd58229
NeverSayNever-Q <69444665+NeverSayNever-Q@users.noreply.github.com>
2022-02-20 22:54:55 +0800

    1、集成Antd Vue 2、前端多环境配置 3、后端跨域配置

---
+++ src/main/java/com/nsn/companion/config/CorsConfig.java
@@ -0,0 +1,17 @@
+package com.nsn.companion.config;
+
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry){
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedHeaders(CorsConfiguration.ALL)
+                .allowedMethods(CorsConfiguration.ALL)
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
--- web/src/main.ts
+++ web/src/main.ts
@@ -2,5 +2,20 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Antd from 'ant-design-vue';
+import 'ant-design-vue/dist/antd.css';
+import * as Icons from '@ant-design/icons-vue';
+import axios from 'axios';
 
-createApp(App).use(store).use(router).mount('#app')
+const app = createApp(App).use(store).use(router).use(Antd)
+
+//全局使用图标
+const icons: any = Icons;
+for(const i in icons){
+    app.component(i, icons[i]);
+}
+
+app.mount('#app')
+
+//配置axios根路径
+axios.defaults.baseURL = process.env.VUE_APP_SERVER;
